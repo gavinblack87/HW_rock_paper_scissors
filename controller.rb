e7008369646662b72fbaa35777a55b44e82c9a44
gavinblack87 <gavin_black@hotmail.co.uk>
2019-07-16 17:18:43 +0100

    hand 1 win

--- controller.rb
+++ controller.rb
@@ -8,6 +8,13 @@ get '/' do
   erb(:home)
 end
 
+get '/win/:hand1/:hand2' do
+  hand1 = params[:hand1]
+  hand2 = params[:hand2]
+  @result = RPSGame.check_win(hand1, hand2)
+  erb(:rock_wins)
+end
+
 get '/scissor-wins' do
   erb(:scissor_wins)
 end
